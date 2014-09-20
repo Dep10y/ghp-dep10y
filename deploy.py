967b10e25d2f7952ae4ec1901bc04e6b9cc6587d
Vacation9 <samuel.damashek@gmail.com>
2014-09-20 14:11:46 -0400

    Fix

--- deploy.py
+++ deploy.py
@@ -22,36 +22,47 @@ def doPost(path, args={}):
     resp = json.loads(requests.post('https://api.digitalocean.com/v2%s' % path, data=json.dumps(args), headers={'Authorization':'Bearer %s' % TOKEN, 'Content-Type':'application/json'}).text)
     return resp
 
-def doDeploy(project_id):
-	projects = firebase.get('/projects')
-	p = None
-	for project in projects:
-		if project_id in projects[project]:
-			p = projects[project][project_id]
-	if not p:
-		print("p doesn't exist")
-		return False
+def doDeploy(project_id, f):
+    projects = firebase.get('/projects')
+    p = None
+    for project in projects:
+        if project_id in projects[project]:
+            p = projects[project][project_id]
+    if not p:
+        print("p doesn't exist")
+        return False
 
-	if 'droplet_id' not in p:
-		print("No droplet id")
-		return False
+    if 'droplet_id' not in p:
+        print("No droplet id")
+        return False
 
-	droplet = doGet('/droplets/%s' % p['droplet_id'])['droplet']
-	files = doGet('/files/%s' % project_id)
-	with tempfile.mkdtemp() as tmpdir:
-		for f in files:
-			fi = open(os.path.join(tmpdir, f['filepath']), 'w')
-			fi.write(f['text'])
-			fi.close()
-		p = subprocess.Popen(['/usr/bin/rsync', '-az', tmpdir, 'web@%s:web' % droplet['name']])
-		print p.wait()
-		print('rsync done!')
-	if p['type'] == 'flask':
-		p = subprocess.Popen(['/usr/bin/ssh', '-t', 'web@%s' % droplet['name'], '"kill `cat ~/.app_pid`; sleep 3; kill -9 `cat ~/.app_pid`; "'])
-		print p.wait()
-		p = subprocess.Popen('/usr/bin/ssh -t web@%s "screen \"python web/app.py\" &; echo $! > ~/.app_pid;"' % droplet['name'], shell=True)
-		print p.wait()
-	else:
-		print('Unsupported type!')
-		return False
+    droplet = doGet('/droplets/%s' % p['droplet_id'])['droplet']
+    files = f
+    with tempfile.mkdtemp() as tmpdir:
+        for f in files:
+            fi = open(os.path.join(tmpdir, f['filepath']), 'w')
+            fi.write(f['text'])
+            fi.close()
+        p = subprocess.Popen(['/usr/bin/rsync', '-az', tmpdir, 'web@%s:web' % droplet['name']])
+        print p.wait()
+        print('rsync done!')
+    if p['type'] == 'flask':
+        p = subprocess.Popen(['/usr/bin/ssh', '-t', 'web@%s' % droplet['name'], '"kill `cat ~/.app_pid`; sleep 3; kill -9 `cat ~/.app_pid`; "'])
+        print p.wait()
+        p = subprocess.Popen('/usr/bin/ssh -t web@%s "screen \"python web/app.py\" &; echo $! > ~/.app_pid;"' % droplet['name'], shell=True)
+        print p.wait()
+    else:
+        print('Unsupported type!')
+        return False
 
+if __name__ == '__main__':
+    firebase = firebase.FirebaseApplication('https://dep10y.firebaseio.com', authentication=None)
+    while True:
+        deploys = firebase.get('/files')
+        if deploys:
+            for i in deploys:
+                val = deploys[i]
+                firebase.delete('/files/%s' % i)
+                thread = Thread(target=doDeploy, args=(i, val))
+                thread.start()
+        time.sleep(3)
